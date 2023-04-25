67c45c1bfb17c51df6dd92e03a3ee49e114af153
Nandlal <shahnandlal49@gmail.com>
2023-04-25 16:58:03 +0530

    completed navbar and footer

--- my-todo/src/Components/Footer/Footer.js
+++ my-todo/src/Components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Wrapper>
+    <Text>&copy; 2023 Nandlal Shah. All Rights Reserved.</Text>
+  </Wrapper>
   )
-}
+};
+const Wrapper = styled.footer`
+  display: flex;
+  margin-top:580px;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  background: #213059;
+`;
+
+const Text = styled.p`
+  font-size: 0.8rem;
+  color: #FFFFFF;
+`;
 
 export default Footer
